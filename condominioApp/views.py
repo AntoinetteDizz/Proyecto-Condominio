1974b788f0ba041e5655be75448d25e862b49f39
Acosta-Saul <145149375+Acosta-Saul@users.noreply.github.com>
2024-03-02 00:32:56 -0400

    acceso a la vista del admin a traves del login

--- condominioApp/views.py
+++ condominioApp/views.py
@@ -1,12 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models  import Users, RoleType
 # Create your views here.
-
 
 # Vista del login
 def index(request):
-  return render(request, 'login.html')
-
-# Vista de registro de usuario
-def register(request):
-  return render(request, 'register.html')
+  if request.method == 'GET':
+    return render(request, 'login.html')
+  else:
+    email = request.POST['email']
+    password = request.POST['contrasena']
+    
+    users = Users.objects.filter(email=email)
+    
+    print(email)
+    print(password)
+    
+    for user in users:
+      if user.email == email and user.pasword == password and user.acess_id == 1:
+        return render(request, 'view_admin.html')
+    
+    return render(request, 'login.html')
+  
+  
+  
+  
